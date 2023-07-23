0ca16bc72f59e66b20edac129e038165d8717a40
Ahmed Abd El-Monem <112562093+Amon3m@users.noreply.github.com>
2023-07-23 23:00:55 +0300

    make product type filter make price range filter

--- app/src/main/java/com/itigradteamsix/snapshop/products/view/CategoryFragment.kt
+++ app/src/main/java/com/itigradteamsix/snapshop/products/view/CategoryFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.marginTop
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.itigradteamsix.snapshop.R
@@ -23,7 +24,7 @@ import com.itigradteamsix.snapshop.products.viewmodel.ProductViewModelFactory
 import kotlinx.coroutines.launch
 
 
-class CategoryFragment : Fragment(), OnProductsClickListener {
+class CategoryFragment : Fragment(), OnProductsClickListener, FilterOptionsListener {
 
     lateinit var binding: FragmentCategoryBinding
     lateinit var productsAdapter: ProductsAdapter
@@ -33,7 +34,14 @@ class CategoryFragment : Fragment(), OnProductsClickListener {
     var sortTitle: Boolean = false
     var sortPrice: Boolean = false
     var asc: Boolean = false
-    var isCat:Boolean=false
+    var isCat: Boolean = false
+    var fromPrice: Int = 0
+    var toPrice: Int = 0
+    var productType: String? = null
+    var isFiltered: Boolean = false
+    var isPrice: Boolean = false
+    var isType: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         productViewModelFactory = ProductViewModelFactory(
@@ -58,11 +66,20 @@ class CategoryFragment : Fragment(), OnProductsClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         collectionId = CategoryFragmentArgs.fromBundle(requireArguments()).collectionId
         isCat = CategoryFragmentArgs.fromBundle(requireArguments()).comeFromCat
 
-        if (isCat){
-            binding.linearLayout.visibility=View.VISIBLE
+
+        if (isCat) {
+            binding.linearLayout.visibility = View.VISIBLE
+
+            when (collectionId) {
+                Utilities.MEN_COLLECTION_ID -> binding.menImageView.setColorFilter(R.color.black)
+                Utilities.SALE_COLLECTION_ID -> binding.menImageView.setColorFilter(R.color.black)
+                Utilities.WOMEN_COLLECTION_ID -> binding.menImageView.setColorFilter(R.color.black)
+                Utilities.KIDS_COLLECTION_ID -> binding.menImageView.setColorFilter(R.color.black)
+            }
         }
 
 
@@ -77,20 +94,54 @@ class CategoryFragment : Fragment(), OnProductsClickListener {
             productViewModel.products.collect {
                 when (it) {
                     is ApiState.Success<*> -> {
-
+                        Log.e(
+                            "Filter option", "fromPrice:Int= $fromPrice" +
+                                    "    var toPrice:Int=$toPrice" +
+                                    "    var productType:String?= $productType" +
+                                    "    var isFiltered:Boolean= $isFiltered" +
+                                    "    var isPrice: Boolean=$isPrice" +
+                                    "    var isType: Boolean=$isType"
+                        )
                         val data = it.data as? ListProductsResponse
 
                         var brands = data?.products
 
                         if (sortTitle) {
                             if (asc) {
+
                                 brands = brands?.sortedBy { it?.title }
+                                if (isFiltered) {
+                                    if (isPrice && !isType) {
+                                        brands = makePriceFilter(fromPrice, toPrice, brands)
+                                    } else if (isType && !isPrice) {
+                                        brands = makeTypeFilter(productType, brands)
+                                    } else {
+                                        brands = makePriceFilter(fromPrice, toPrice, brands)
+                                        brands = makeTypeFilter(productType, brands)
+
+                                    }
+
+                                }
+
                                 productsAdapter.submitList(null)
 
                                 productsAdapter.submitList(brands)
 
                             } else {
                                 brands = brands?.sortedByDescending { it?.title }
+                                if (isFiltered) {
+                                    if (isPrice && !isType) {
+                                        brands = makePriceFilter(fromPrice, toPrice, brands)
+                                    } else if (isType && !isPrice) {
+                                        brands = makeTypeFilter(productType, brands)
+                                    } else {
+                                        brands = makePriceFilter(fromPrice, toPrice, brands)
+                                        brands = makeTypeFilter(productType, brands)
+
+                                    }
+
+                                }
+
                                 productsAdapter.submitList(null)
 
                                 productsAdapter.submitList(brands)
@@ -98,26 +149,64 @@ class CategoryFragment : Fragment(), OnProductsClickListener {
                             }
                         } else if (sortPrice) {
                             if (asc) {
-                                brands = brands?.sortedBy { it?.variants?.get(0)?.price?.toDouble() }
+                                brands =
+                                    brands?.sortedBy { it?.variants?.get(0)?.price?.toDouble() }
+                                if (isFiltered) {
+                                    if (isPrice && !isType) {
+                                        brands = makePriceFilter(fromPrice, toPrice, brands)
+                                    } else if (isType && !isPrice) {
+                                        brands = makeTypeFilter(productType, brands)
+                                    } else {
+                                        brands = makePriceFilter(fromPrice, toPrice, brands)
+                                        brands = makeTypeFilter(productType, brands)
+
+                                    }
+
+                                }
+
                                 productsAdapter.submitList(null)
 
                                 productsAdapter.submitList(brands)
 
                             } else {
-                                brands = brands?.sortedByDescending { it?.variants?.get(0)?.price?.toDouble() }
+                                brands =
+                                    brands?.sortedByDescending { it?.variants?.get(0)?.price?.toDouble() }
+
+                                if (isFiltered) {
+                                    if (isPrice && !isType) {
+                                        brands = makePriceFilter(fromPrice, toPrice, brands)
+                                    } else if (isType && !isPrice) {
+                                        brands = makeTypeFilter(productType, brands)
+                                    } else {
+                                        brands = makePriceFilter(fromPrice, toPrice, brands)
+                                        brands = makeTypeFilter(productType, brands)
+
+                                    }
+
+                                }
+
                                 productsAdapter.submitList(null)
                                 productsAdapter.submitList(brands)
 
                             }
 
                         } else {
+                            if (isFiltered) {
+                                if (isPrice && !isType) {
+                                    brands = makePriceFilter(fromPrice, toPrice, brands)
+                                } else if (isType && !isPrice) {
+                                    brands = makeTypeFilter(productType, brands)
+                                } else {
+                                    brands = makePriceFilter(fromPrice, toPrice, brands)
+                                    brands = makeTypeFilter(productType, brands)
+
+                                }
+
+                            }
+
                             productsAdapter.submitList(brands)
 
                         }
-
-                        Log.e("collectionId", "${brands?.get(0)?.title}")
-
-//                        binding.catRecycl.scrollToPosition(1)
 
 
                     }
@@ -134,24 +223,49 @@ class CategoryFragment : Fragment(), OnProductsClickListener {
 
             }
         }
+
+        binding.fab.setOnClickListener {
+
+            val filterFragment = FilterBottomFragment()
+            filterFragment.setFilterOptionsListener(this) // Set the listener
+            filterFragment.show(childFragmentManager, filterFragment.tag)
+        }
         binding.babyCard.setOnClickListener {
             collectionId = Utilities.KIDS_COLLECTION_ID
             productViewModel.getProducts(requireContext(), collectionId)
+            isPrice = false
+            isType = false
+            isFiltered = false
+            selectEffect()
+
         }
         binding.saleCard.setOnClickListener {
             collectionId = Utilities.SALE_COLLECTION_ID
             productViewModel.getProducts(requireContext(), collectionId)
+            isPrice = false
+            isType = false
+            isFiltered = false
+            selectEffect()
 
         }
         binding.menCard.setOnClickListener {
             collectionId = Utilities.MEN_COLLECTION_ID
 
             productViewModel.getProducts(requireContext(), collectionId)
+            isPrice = false
+            isType = false
+            isFiltered = false
+            selectEffect()
 
         }
         binding.womenCard.setOnClickListener {
             collectionId = Utilities.WOMEN_COLLECTION_ID
             productViewModel.getProducts(requireContext(), collectionId)
+            isPrice = false
+            isType = false
+            isFiltered = false
+            selectEffect()
+
         }
 
         binding.sortTitle.setOnClickListener {
@@ -193,4 +307,86 @@ class CategoryFragment : Fragment(), OnProductsClickListener {
     override fun onWishClick(product: ProductsItem?) {
         TODO("Not yet implemented")
     }
+
+    override fun onFilterOptionsSelected(
+        minPrice: Int,
+        maxPrice: Int,
+        type: String?,
+        isFilter: Boolean,
+        isP: Boolean,
+        isT: Boolean
+    ) {
+        fromPrice = minPrice
+        toPrice = maxPrice
+        productType = type
+        isFiltered = isFilter
+        isPrice = isP
+        isType = isT
+
+        productViewModel.getProducts(requireContext(), collectionId)
+
+        Log.e("filter", "max: $fromPrice min: $toPrice productType: $type")
+    }
+
+    fun makePriceFilter(
+        fromPrice: Int,
+        toPrice: Int,
+        product: List<ProductsItem?>?
+    ): List<ProductsItem?>? {
+
+        return product?.filter { product ->
+            val price = product?.variants?.get(0)?.price?.toDouble() ?: 0.0
+
+            price >= fromPrice && price <= toPrice
+        }
+    }
+
+    fun makeTypeFilter(type: String?, product: List<ProductsItem?>?): List<ProductsItem?>? {
+
+        return product?.filter { product ->
+            val typeOfProduct = product?.productType
+
+            typeOfProduct == type
+        }
+    }
+
+    fun selectEffect() {
+        when (collectionId) {
+            Utilities.MEN_COLLECTION_ID -> {
+                binding.menImageView.setColorFilter(R.color.black)
+                binding.womenImageView.clearColorFilter()
+                binding.babyImageView.clearColorFilter()
+                binding.saleImageView.setBackgroundColor(getResources().getColor(R.color.md_theme_dark_inversePrimary))
+
+
+            }
+
+            Utilities.SALE_COLLECTION_ID -> {
+                binding.saleImageView.setBackgroundColor(getResources().getColor(R.color.black))
+                binding.womenImageView.clearColorFilter()
+                binding.babyImageView.clearColorFilter()
+                binding.menImageView.clearColorFilter()
+
+
+            }
+
+            Utilities.WOMEN_COLLECTION_ID -> {
+                binding.womenImageView.setColorFilter(R.color.black)
+                binding.babyImageView.clearColorFilter()
+                binding.menImageView.clearColorFilter()
+                binding.saleImageView.setBackgroundColor(getResources().getColor(R.color.md_theme_dark_inversePrimary))
+
+            }
+
+            Utilities.KIDS_COLLECTION_ID -> {
+                binding.babyImageView.setColorFilter(R.color.black)
+                binding.womenImageView.clearColorFilter()
+                binding.menImageView.clearColorFilter()
+                binding.saleImageView.setBackgroundColor(getResources().getColor(R.color.md_theme_dark_inversePrimary))
+
+
+            }
+        }
+    }
+
 }
